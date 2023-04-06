0316934bfede91eb60aec589d7199cbb16beff09
Alexey Telnov <at@pryaniky.ru>
2023-04-06 23:42:36 +0300

    add random string

--- test_HW2.py
+++ test_HW2.py
@@ -1,12 +1,21 @@
+import random
+import string
 
 from selene.support.shared import browser
 from selene import be, have
 
+
+def generate_random_string(length):
+    letters = string.ascii_lowercase
+    rand_string = ''.join(random.choice(letters) for i in range(length))
+    return rand_string
 
 def test_google_search(set_up_browser):
     browser.element('[name="q"]').should(be.blank).type('yashaka/selene').press_enter()
     browser.element('[id="search"]').should(have.text('Selene - User-oriented Web UI browser tests in Python'))
 
 def test_google_nullsearch(set_up_browser):
-    browser.element('[name="q"]').should(be.blank).type('fgvyhercg').press_enter()
+    length = random.randint(10, 15)
+    rand_string = generate_random_string(length)
+    browser.element('[name="q"]').should(be.blank).type(rand_string).press_enter()
     browser.element('#result-stats').should(have.text('Результатов: примерно 0'))
