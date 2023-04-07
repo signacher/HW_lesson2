08339d134563acf35b2e16cae243cecff96ed151
Alexey Telnov <at@pryaniky.ru>
2023-04-07 13:01:23 +0300

    add correntions

--- test_hw2.py
+++ test_hw2.py
@@ -7,12 +7,14 @@ from selene import be, have
 
 def generate_random_string(length):
     letters = string.ascii_lowercase
-    rand_string = ''.join(random.choice(letters) for i in range(length))
+    rand_string = ''.join(random.choice(letters) for _ in range(length))
     return rand_string
+
 
 def test_google_search(set_up_browser):
     browser.element('[name="q"]').should(be.blank).type('yashaka/selene').press_enter()
     browser.element('[id="search"]').should(have.text('Selene - User-oriented Web UI browser tests in Python'))
+
 
 def test_google_nullsearch(set_up_browser):
     length = random.randint(10, 15)
